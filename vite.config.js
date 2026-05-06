eb589c03386fe9a7d33aaa0c19cbc73c457c32fb
goncaloguardado-blip <goncalo.guardado@gmail.com>
2026-05-06 10:51:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
-      manifest: {
-        name: 'Slide & Stone SA',
-        short_name: 'Slide & Stone',
-        description: 'Gestão de obras, orçamentos, equipa e estaleiro',
-        theme_color: '#18181b',
-        background_color: '#fafaf9',
-        display: 'standalone',
-        orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        maximumFileSizeToCacheInBytes: 5000000
-      }
-    })
-  ],
+  plugins: [react()],
+  publicDir: '.',
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    chunkSizeWarningLimit: 1500
-  },
-  server: {
-    port: 5173,
-    host: true
+    rollupOptions: {
+      input: 'index.html'
+    }
   }
 });
